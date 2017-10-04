4acd2638e91a2e81b504a5232b6ad46f65f77de1
Michael Winberry <mwinberry0101@gmail.com>
2017-10-03 18:03:19 -0700

    made circle paint a line while changing colors

--- sketch.js
+++ sketch.js
@@ -1,10 +1,20 @@
 //Zombulator by michael Winberry
 
+var x,y;
 function setup() {
     createCanvas(800,800);
+    x = width;
+    y = height;
 };
 
 function draw() {
-         fill(random(255),random(255),random(255));
-        ellipse(random(800),random(800),50,50);
+    fill(random(255),random(255),random(255));
+    ellipse(x/2,y,50,50);
+        
+    if (y < 0) {
+        y = height;
+    } else {
+        y = y - 2;
+    };
+
 };
