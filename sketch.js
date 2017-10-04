fa71d2bfe341462dc07f7f877352b0b1e82f8c80
Michael Winberry <mwinberry0101@gmail.com>
2017-10-03 18:26:58 -0700

    Set circle to move with mouse using mouseX and mouseY as ellipse coordinates

--- sketch.js
+++ sketch.js
@@ -1,24 +1,10 @@
 //Zombulator by michael Winberry
 
-var x,y;
 function setup() {
     createCanvas(800,800);
-    x = width;
-    y = height;
 };
 
 function draw() {
     fill(random(255),random(255),random(255));
-    ellipse(x,y,50,50);
-        
-    if (y < 0) {
-        y = height;
-        x = x - 50;
-    } else {
-        y = y - 2;
-    };
-    if (x < 0) {
-        x = width;
-    };
-
+    ellipse(mouseX,mouseY,50,50);
 };
