6c00e3e9fa534920c6d236b9b00fed0225070833
mwinb <mwinberry0101@gmail.com>
2017-10-08 19:48:33 -0700

    Made circle with random border color move across the creen changing colors each time.

--- sketch.js
+++ sketch.js
@@ -1,12 +1,38 @@
 //Zombulator by michael Winberry
 
+var zombieX,zombieY;
+var r,b,g;
+var sr,sb,sg;
+
+ 
 function setup() {
     createCanvas(800,800);
+    zombieX = width;
+    r = 135
+   b = 232;
+   g = 175;
+   sr = random(255);
+   sb = random(255);
+   sg = random(255);
+   sw = random(15,25);
 };
 
 function draw() {
-    fill(random(255),random(255),random(255));
-    if (mouseIsPressed) {
-        ellipse(mouseX,mouseY,50,50);
+    background(255,255,255);
+    stroke(sr,sb,sg);
+    strokeWeight(sw);
+    fill(r,b,g);
+    ellipse(zombieX,height/2,80,80);
+    if (zombieX > width) {
+        zombieX = 0;
+        r = random(255);
+        g = random(255);
+        b = random(255);
+        sr = random(255);
+        sb = random(255);
+        sg = random(255);
+        sw = random(15,25);
+    } else {
+        zombieX = zombieX + 4;
     };
 };
