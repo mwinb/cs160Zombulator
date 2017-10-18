396a5e4772802d756e15554b7e6ca3b77148ee66
mwinb <mwinberry0101@gmail.com>
2017-10-18 10:46:38 -0700

    Updated all ellipse parameters for Humans and Zombies to be stored in separate arrays in order to iterate and call creating multiple ellipses for each. MW

--- sketch.js
+++ sketch.js
@@ -3,59 +3,65 @@
 
 var backgroundColor;
 
-const MIN_SIZE = 25; // old browser? change to var.
-const MAX_SIZE = 150;
+const MIN_SIZE = 10; // old browser? change to var.
+const MAX_SIZE = 30;
 const NUM_ZOMBIES = 300;
 const NUM_HUMANS = 500;
 
+var humanXs = [];
+var humanColors = [];
+var humanSizes = [];
+var humanYs = [];
 
-var zombieX;
-var zombieY;
-var zombieSize;
-var zombieColor;
-
-var humanX;
-var humanY;
-var humanSize;
-var humanColor;
+var zombieColors = [];
+var zombieSizes = [];
+var zombieXs = [];
+var zombieYs = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   backgroundColor = color(245, 255, 245);
-  initializeZombie();
-  initializeHuman();
+  initializeZombies();
+  initializeHumans();
 }
- 
+
 function draw() {
   background(backgroundColor);
   noStroke();
- 
-  drawZombie();
-  drawHuman();
+  
+  drawZombies();
+  drawHumans();
 }
 
-function initializeZombie() {
-  zombieX = random(0, windowWidth);
-  zombieY = random(0, 200);
-  zombieSize = random(MIN_SIZE, MAX_SIZE);
-  zombieColor = color(random(50, 255), random(50, 255), random(50, 255), 150);
+function initializeZombies() {
+  for (var i = 0; i < NUM_ZOMBIES; ++i) {
+    zombieXs.push(random( 0, windowWidth));
+    zombieYs.push(random(0,200));
+    zombieSizes.push(random(MIN_SIZE, MAX_SIZE));
+    zombieColors.push(color(random(50,255), random(50,255), random(50, 255), 150));
+  }
 }
 
-function initializeHuman() {
+function initializeHumans() {
   // raaaawwwwwrrr!
-  humanX = random(0, windowWidth);
-  humanY = random(windowHeight - 200, windowHeight);
-  humanSize = random(MIN_SIZE, MAX_SIZE);
-  humanColor = color(random(0,155), random(155,255), 0, 200);
+  for (var i = 0; i < NUM_HUMANS; ++i) {
+    humanXs.push(random(0, windowWidth));
+    humanYs.push(random(windowHeight - 200, windowHeight));
+    humanSizes.push(random(MIN_SIZE, MAX_SIZE));
+    humanColors.push(color(random(0,155), random(155,255), 0, 200));
+  }
 }
 
-function drawZombie() {
-  fill(zombieColor);
-  ellipse(zombieX, zombieY, zombieSize, zombieSize);
+function drawZombies() {
+  for ( var i = 0; i < NUM_ZOMBIES; ++i ) { 
+    fill(zombieColors[i]);
+    ellipse(zombieXs[i], zombieYs[i], zombieSizes[i], zombieSizes[i]);
+  }
 }
 
-function drawHuman() {
-  // mmmm braaaiiiiinss!
-  fill(humanColor);
-  ellipse(humanX, humanY, humanSize, humanSize);
+function drawHumans() {
+  for ( var i = 0; i < NUM_HUMANS; ++i ) {
+    fill(humanColors[i]);
+    ellipse(humanXs[i], humanYs[i], humanSizes[i], humanSizes[i]);
+  }
 }
