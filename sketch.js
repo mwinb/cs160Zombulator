8cc6b432d1f072bbb7e2c92d42f3bd0326c6e13c
mwinb <mwinberry@live.com>
2017-11-07 13:36:42 -0800

    Created initializeDeath() function that creates a smaller circle that falls to bottom of the screen after human or zombie dies. Next commit: create death arrray that populates multiple death objects and falls to bottom screem. MW

--- sketch.js
+++ sketch.js
@@ -15,7 +15,7 @@ var humanCount = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  backgroundColor = color(245, 255, 245);
+  backgroundColor = color(0, 0, 0);
   initializePopulation();
 }
 
@@ -63,8 +63,9 @@ function movePopulation() {
 
 function initializeZombie() {
   return {
-    type: "Zombie",
-    alive: true,
+    type: function() {
+      return "Zombie"
+    },
     x: random(0, windowWidth),
     y: random(0, 200),
     speed: random(0.25, 3),
@@ -73,21 +74,24 @@ function initializeZombie() {
     move: function() {
       this.y += this.speed;
       this.x -= random(-2,2);
-      this.y += random(-1,1)
+      this.y += random(-2,2)
     },
     draw: function() {
+      if (this.y <= 0 || this.y >= windowHeight) {
+        this.speed = -this.speed;
+      }
       if (this.x + this.size/2 > windowWidth) {
-        this.x = this.x - this.size/2;
+        this.x = this.x - this.size;
       }
 
       if (this.x + this.size/2 < 0 ) {
-        this.x = this.x + this.size/2;
+        this.x = this.x + this.size;
       }
 
-      if (this.alive == true) {
-        fill(this.color);
-        ellipse(this.x, this.y, this.size, this.size);
-      }
+ 
+      fill(this.color);
+      ellipse(this.x, this.y, this.size, this.size);
+      
     },
     isTouching: function(food) {
       var distance = dist(this.x, this.y, food.x, food.y);
@@ -100,8 +104,9 @@ function initializeZombie() {
 
 function initializeHuman() {
   return {
-    type: "Human",
-    alive: true,
+    type: function() {
+      return "Human"
+    },
     x: random(0, windowWidth),
     y: random(windowHeight - 200, windowHeight),
     speed: random(0.25, 3),
@@ -113,23 +118,56 @@ function initializeHuman() {
       this.y -= random(-1,1)
     },
     draw: function() {
+      if (this.y <= 0 || this.y >= windowHeight) {
+        this.speed = -this.speed;
+      }
       if (this.x + this.size/2 > windowWidth) {
-        this.x = this.x - this.size/2;
+        this.x = this.x - this.size;
       }
 
       if (this.x + this.size/2 < 0 ) {
-        this.x = this.x + this.size/2;
+        this.x = this.x + this.size;
       }
-      if (this.alive == true) {
-          fill(this.color);
-          ellipse(this.x, this.y, this.size, this.size);
-        }
+
+      fill(this.color);
+      ellipse(this.x, this.y, this.size, this.size);
+
     },
     isTouching: function(zombie) {
       var distance = dist(this.x, this.y, zombie.x, zombie.y);
       if (distance <= this.size/2 + zombie.size/2) {
         return true;
       }
+    }
+  }
+}
+
+function initializeDeath() {
+  return {
+    type: function () {
+      return "Dead";
+    },
+    x: 0,
+    y: 0,
+    speed: random(1,3),
+    size: random(3,5),
+    color: color(0,0,0),
+    move: function() {
+      this.y += this.speed;
+      this.x -= random(-2,2);
+      this.y += random(-2,2)
+    },
+    draw: function () {
+      if (this.x + this.size/2 > windowWidth) {
+        this.x = this.x - this.size;
+      }
+
+      if (this.x + this.size/2 < 0 ) {
+        this.x = this.x + this.size;
+      }
+
+      fill(this.color);
+      ellipse(this.x, this.y, this.size, this.size);
     }
   }
 }
@@ -141,46 +179,46 @@ function checkCollision () {
     for (var j = i+1; j < POPULATION_SIZE; ++j) {
       var victim = population[j];
       
-      if ( attacker.type != victim.type && attacker.isTouching(victim) == true && 
-        attacker.alive == true && victim.alive == true) {
+      if ( attacker.type() != victim.type() && attacker.type() != "Dead" && 
+        victim.type() != "Dead" && attacker.isTouching(victim) == true) {
         
         var roll = random(0,100);
         
-        if (roll < 45) {
+        if (roll < 50) {
           
-          if(attacker.type == "Zombie") {
-            attacker.alive = false;
+          if(attacker.type() == "Zombie") {
+            turnDead(i);
             --zombieCount;
           
           } else {
-            victim.alive = false;
+            turnDead(j);
             --zombieCount;
           }
 
-        } else if (roll < 65) {
-          
-          if (attacker.type == "Human") {
-            turnHuman(i)
-            --humanCount;
-            ++zombieCount;
-          
-          } else {
-            turnHuman(j);
-            --humanCount;
-            ++zombieCount;
-          }
+        } else {
 
-        } else if (roll < 100) 
+          if (roll < 60) {
           
-          if(attacker.type == "Human") {
-            attacker.alive = false;
+            if (attacker.type() == "Human") {
+              turnHuman(i);
+              --humanCount;
+              ++zombieCount;
+
+            } else {
+              turnHuman(j);
+              --humanCount;
+              ++zombieCount;
+              } 
+          } else if (attacker.type() == "Human") {
+            turnDead(i);
             --humanCount;
           
           } else {
-            victim.alive = false;
+            turnDead(j);
             --humanCount;
           }
         }
+      }
     }
   }
 }
@@ -190,4 +228,12 @@ function turnHuman(humanIndex) {
   tempZombie.x = population[humanIndex].x;
   tempZombie.y = population[humanIndex].y;
   population[humanIndex] = tempZombie;
+}
+
+function turnDead(popIndex) {
+  var tempDead = initializeDeath();
+  tempDead.x = population[popIndex].x;
+  tempDead.y = population[popIndex].y;
+  tempDead.color = population[popIndex].color;
+  population[popIndex] = tempDead;
 }
